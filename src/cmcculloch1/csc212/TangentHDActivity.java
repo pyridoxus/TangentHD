4545e442380aa3f46dab5bad5fc6d99114ed6bb4
Craig McCulloch <pyridoxus@null.net>
2012-03-03 10:08:36 -0600

    Initial commit

---
+++ src/cmcculloch1/csc212/TangentHDActivity.java
@@ -0,0 +1,13 @@
+package cmcculloch1.csc212;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TangentHDActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
