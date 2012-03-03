211f84b928263c1b37f2edc202c95924f2965075
Craig McCulloch <pyridoxus@null.net>
2012-03-03 11:56:47 -0600

    Menu test

--- src/cmcculloch1/csc212/TangentHDActivity.java
+++ src/cmcculloch1/csc212/TangentHDActivity.java
@@ -2,6 +2,8 @@ package cmcculloch1.csc212;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class TangentHDActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,11 @@ public class TangentHDActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    
 }
