2c0cbe999b9860c36ad22a99d41a55fbc7c4ed2f
Craig McCulloch <pyridoxus@null.net>
2012-03-04 06:31:28 -0600

    Context menu works. Will change it for selecting colors.

--- src/cmcculloch1/csc212/TangentHDActivity.java
+++ src/cmcculloch1/csc212/TangentHDActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -19,46 +18,49 @@ public class TangentHDActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ImageView graph = (ImageView) findViewById(R.drawable.example_screen);
+        ImageView graph = (ImageView) findViewById(R.id.exampleScreen);
         registerForContextMenu(graph);
     }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+    
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.context_menu, menu);
-    }    
+    }
+    
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
         Context context = getApplicationContext();
         CharSequence text = "TangentHD";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
+        int m_id = item.getItemId();
 
-        switch (item.getItemId()) {
-            case R.id.equation1:
+        switch (m_id) {
+            case R.id.m_equation1:
             	// Will do something else here in case statement in future
-            	text = "Selected equation " + info.id;
+            	text = "Selected equation " + m_id;
                 toast = Toast.makeText(context, text, duration);
                 toast.show();
                 return true;
-            case R.id.equation2:
+            case R.id.m_equation2:
             	// Will do something else here in case statement in future
-            	text = "Selected equation " + info.id;
+            	text = "Selected equation " + m_id;
                 toast = Toast.makeText(context, text, duration);
                 toast.show();
                 return true;
-            case R.id.equation3:
+            case R.id.m_equation3:
             	// Will do something else here in case statement in future
-            	text = "Selected equation " + info.id;
+            	text = "Selected equation " + m_id;
                 toast = Toast.makeText(context, text, duration);
                 toast.show();
                 return true;
