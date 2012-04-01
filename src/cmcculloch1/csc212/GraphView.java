ddd6ab8612f1c439647d75c23d7cccec406262c6
Craig McCulloch <pyridoxus@null.net>
2012-04-01 07:02:16 -0500

    Still trying to fix attributes.

--- src/cmcculloch1/csc212/GraphView.java
+++ src/cmcculloch1/csc212/GraphView.java
@@ -7,8 +7,9 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class GraphView extends View {
-	Bitmap bg;	// Background bitmap to hold the more static image of graph
-	Bitmap fg;	// Foreground bitmap to hold the more dynamic image of secants
+	Bitmap bg = null;	// Background bitmap holds more static image of graph
+	Bitmap fg = null;	// Foreground bitmap holds more dynamic image of secants
+	private GraphAttributes ga;
 
 	public GraphView(Context context) {
 		// TODO Auto-generated method stub
@@ -18,23 +19,26 @@ public class GraphView extends View {
 	public GraphView(Context context, AttributeSet attrs) {
 		// TODO Auto-generated method stub
 		super(context, attrs);
+		init(context, attrs);
 	}
 
 	public GraphView(Context context, AttributeSet attrs, int defStyle) {
 		// TODO Auto-generated method stub
 		super(context, attrs, defStyle);
+		init(context, attrs);
 	}
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		createBMPs();
+		//createBMPs();
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
+		if(bg == null && fg == null) createBMPs();
 	}
 
 	@Override
@@ -62,5 +66,9 @@ public class GraphView extends View {
 		bg = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(),
 								Bitmap.Config.ARGB_8888);
 		fg = Bitmap.createBitmap(bg);
+	}
+	
+	private void init(Context context, AttributeSet attrs) {
+		ga = new GraphAttributes(context, attrs);
 	}
 }
